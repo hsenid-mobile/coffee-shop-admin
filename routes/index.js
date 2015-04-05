e2e9a986251fd763b831ffb6ea63c1aa635ee026
isuruanu <isuru.develop@gmail.com>
2015-04-05 18:45:03 +0530

    part 1.0

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var yeast = require('yeast');
 
 var express = require('express');
 var router = express.Router();
+var tapApi = require("tap-telco-api");
 
 var feedbackManager = require('./../backend_manager').feedbackRepo;
 
@@ -16,6 +17,13 @@ router.get('/', function(req, res, next) {
   feedbackManager.getAll(function(result){
     res.render('index', { title: 'Feedback', feedback : result, record_no : 1})
   })
+});
+
+router.post('/sms', function(req, res, next){
+  res.send(tapApi.sms.successResponse);
+  next()
+}, function(req, res){
+    feedbackManager.saveToRepo({key: yeast(), message:req.body.message, mobileNo:req.body.sourceAddress, date:new Date()})
 });
 
 module.exports = router;
