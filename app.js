52c7a9bf82aaa8970b75aab84764037b93ab4ed0
isuruanu <isuru.develop@gmail.com>
2015-04-06 00:09:16 +0530

    minor refactoring

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ var promotions = require('./routes/promotions');
 var payments = require('./routes/payments');
 var flavours = require('./routes/flavours');
 
+
+/*init data*/
+var flavorsRepo = require('./backend_manager').flavorsRepo
+
+flavorsRepo.saveToRepo({key : "1", name : "Espresso", price : "285Rs"});
+flavorsRepo.saveToRepo({key : "2", name : "Cappuccino", price : "310Rs"});
+flavorsRepo.saveToRepo({key : "3", name : "Americano", price : "290Rs"});
+flavorsRepo.saveToRepo({key : "4", name : "Caffe Latte", price : "415Rs"});
+flavorsRepo.saveToRepo({key : "5", name : "Caf au Lait", price : "395Rs"});
+flavorsRepo.saveToRepo({key : "6", name : "Mochachino", price : "520Rs"});
+flavorsRepo.saveToRepo({key : "7", name : "Caramel Macchiato", price : "495Rs"});
+
 var app = express();
 
 // view engine setup
