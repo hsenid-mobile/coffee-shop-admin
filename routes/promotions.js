fa3c7a1dc2db9c033674a25ccd81031369d7cb68
isuruanu <isuru.develop@gmail.com>
2015-04-05 23:15:59 +0530

    part 2.0

--- routes/promotions.js
+++ routes/promotions.js
@@ -1,14 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var tapApi = require("tap-telco-api");
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.render('promotions', { title: 'Promotions' });
 });
 
-router.post('/send', function(req, res) {
-    console.log("Send button pressed." + req.body.messageInput)
-    res.redirect("/promotions")
+router.post('/send',
+    function(req, res, next) {
+        res.redirect("/promotions")
+        next()
+    },
+    function(req, res){
+        tapApi.sms.requestCreator({applicationId : "APP_000101", password : "password"}).broadcast(req.body.messageInput, function(mtReq){
+            tapApi.transport.createRequest({hostname: '127.0.0.1', port: 7000, path: '/sms/send'}, mtReq, function(request){
+                tapApi.transport.httpClient(request, function() {
+                    console.log("Mt request send to subscriber" + mtReq)
+                })
+            })
+    })}
+);
+
+router.post('/subscription', function(req, res){
+    console.log("Subscription notification " + req.body.subscriberId)
+    res.send(tapApi.subscription.subscriptionNotificationResponse)
 });
 
 module.exports = router;
